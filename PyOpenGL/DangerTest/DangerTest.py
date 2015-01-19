09bef5c0df32994d7882fa5f46b79aba09f67155
deadmeat <robin.t.potter@googlemail.com>
2015-01-19 22:58:57 +0000

    danger code complete

--- PyOpenGL/DangerTest/DangerTest.py
+++ PyOpenGL/DangerTest/DangerTest.py
@@ -12,9 +12,10 @@ colours={}
 colours["red"]=[1.0,0.0,0.0,1.0]
 colours["green"]=[0.0,1.0,0.0,1.0]
 colours["blue"]=[0.0,0.0,1.0,1.0]
-#colours["yellow"]=[1.0,1.0,0.0,1.0]
-#colours["cyan"]=[0.0,1.0,1.0,1.0]
-#colours["white"]=[1.0,1.0,1.0,1.0]
+colours["yellow"]=[1.0,1.0,0.0,1.0]
+colours["cyan"]=[0.0,1.0,1.0,1.0]
+colours["pink"]=[1.0,0.0,1.0,1.0]
+colours["white"]=[1.0,1.0,1.0,1.0]
   
 class Thing:
 
@@ -23,6 +24,11 @@ class Thing:
     xx,yy,zz=0,0,10
     cxx,cyy,czz=0,0,0
     X=0
+    
+    xRot=0
+    yRot=0
+    mPos=[0,0]
+    speed=0.1
 
     def animate(self,FPS=1):
         
@@ -42,18 +48,17 @@ class Thing:
         self.topFPS=int(1000/drawTime)
         if int(100*time())%100==0: print "draw time "+str(drawTime)+" top FPS "+str(1000/drawTime)
         
-    def draw(Self):
-    
-        
-        glPushMatrix()     
-        ##for t in self.tiles: t.draw()
+    def draw(self):
+            
+	glTranslate(0,0,8)
+	glRotate(-self.yRot,1,0,0)
+	glRotate(-self.xRot,0,1,0)
+	    
+	    
         f=open("DangerCode.txt","r")
         code=f.read()
         print "DC:"+code
         exec(code)
-        
-        glPopMatrix()
-                 
 
     def __init__(self):   
      
@@ -76,7 +81,8 @@ class Thing:
         glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
         glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
         glEnable(GL_LIGHT0)
-        
+        glutMotionFunc(self.mousedrag)
+        glutMouseFunc(self.mouse)
         '''
         glutSpecialFunc(self.keydownevent)
         glutSpecialUpFunc(self.keyupevent)
@@ -87,16 +93,27 @@ class Thing:
         glutDisplayFunc(self.display)
         #glutIdleFunc(self.display)
         glMatrixMode(GL_PROJECTION)
-        gluPerspective(110.0,640.0/480.,1.,50.)
+        gluPerspective(60.0,640.0/480.,1.,50.)
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         
-        self.animate(FPS=20)
+        self.animate(FPS=15)
         
         glutMainLoop()
         
         
         return
+
+    def mousedrag(self,x,y):
+	print ((x,y))
+	self.xRot+=(self.mPos[0]-x)*self.speed
+	self.yRot+=(self.mPos[1]-y)*self.speed
+	self.mPos=[x,y]
+	
+	
+    def mouse(self,button,state,x,y):
+	print ((button,state,x,y))
+	self.mPos=[x,y]
 
     def display(self):
 
@@ -110,9 +127,7 @@ class Thing:
                   self.cxx,self.cyy,self.czz,
                   0,1,0)          
         
-        glPushMatrix()     
         self.draw()
-        glPopMatrix()
     
     
         glutSwapBuffers()
