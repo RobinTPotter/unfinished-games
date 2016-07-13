c4bf38258216302c79ee95764d9829ef3e25f862
RobinTPotter <r@p.com>
2016-07-13 17:07:48 +0100

    added myappcam1b

--- myappcam1/src/com/example/myappcam/OddButton.java
+++ myappcam1/src/com/example/myappcam/OddButton.java
@@ -17,12 +17,12 @@ public class OddButton extends View {
 
 
     Bitmap bmp;
-    String timeupdate="not set";
+    String timeupdate = "not set";
     Bitmap bck;
 
     public void setBmp(Bitmap bmp) {
         this.bmp = bmp;
-        this.timeupdate=(new Date()).toString();
+        this.timeupdate = (new Date()).toString();
     }
 
     public OddButton(Context context) {
@@ -42,53 +42,34 @@ public class OddButton extends View {
     public void draw(Canvas c) {
         super.draw(c);
 
-        Paint p=new Paint() ;
+        Paint p = new Paint();
         p.setColor(Color.BLUE);
         p.setStyle(Paint.Style.STROKE);
-        p.setStrokeWidth(0.2f);
+        p.setStrokeWidth(0.7f);
 
-        Paint bp=new Paint() ;
-        bp.setARGB(20,20,0,0);
+        Paint bp = new Paint();
+        bp.setARGB(20, 20, 0, 0);
 
-        Paint bp2=new Paint() ;
-        bp2.setARGB(0,0,0,0);
+        Paint bp2 = new Paint();
+        bp2.setARGB(0, 0, 0, 0);
 
-        c.drawRect (0,0,getWidth()-1,getHeight()-1,p);
-        c.drawRect(10,10, getWidth()-10,getHeight()-10,p);
+        c.drawRect(0, 0, getWidth() - 1, getHeight() - 1, p);
+        c.drawRect(10, 10, getWidth() - 10, getHeight() - 10, p);
 
-        if (bmp!=null) {
+        if (bmp != null) {
 
-            c.drawText("Hello",10,10,p);
-            c.drawText(String.valueOf(bmp.getWidth()),10,30,p);
-            c.drawText(String.valueOf(bmp.getHeight()),10,50,p);
+            c.drawBitmap(bmp, new Rect(0, 0, bmp.getWidth(), bmp.getHeight()), new Rect(0, 0, getWidth(), getHeight()), bp);
 
-            int sq=20;
-
-            for (int xx=0;xx<getWidth();xx+=sq) {
-                for (int yy=0;yy<getHeight();yy+=sq) {
-
-                    Rect r=new Rect(xx,yy,xx+sq,yy+sq);
-                    c.drawBitmap(bmp,r,r,bp);
-
-                }
+            c.drawText("Hello", 10, 10, p);
+            c.drawText(String.valueOf(bmp.getWidth()), 10, 30, p);
+            c.drawText(String.valueOf(bmp.getHeight()), 10, 50, p);
 
 
 
-
-
-            }
-
-
-
-          //  c.drawBitmap(bmp,new Rect(0,0,bmp.getWidth(),bmp.getHeight()),new Rect(0,0,getWidth(),getHeight()),bp);
-
-
-
-           // c.drawBitmap(bmp,0,0,null);
         }
 
-        c.drawText("Hello",20,20,p);
-        c.drawText(timeupdate,20,40,p);
+        c.drawText("Hello", 20, 20, p);
+        c.drawText(timeupdate, 20, 40, p);
 
     }
 
