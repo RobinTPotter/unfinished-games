dce341292a954fd274ce7d691a222ca013e77315
deadmeat <robin.t.potter@googlemail.com>
2016-07-15 20:54:34 +0100

    a1

---
+++ myappcam1/bin/stopmotioncamera-release-unsigned.apk.d
@@ -0,0 +1,3 @@
+/home/deadmeat/Spod/unfinished-games/myappcam1/bin/stopmotioncamera-release-unsigned.apk : \
+/home/deadmeat/Spod/unfinished-games/myappcam1/bin/stopmotioncamera.ap_ \
+/home/deadmeat/Spod/unfinished-games/myappcam1/bin/classes.dex \
