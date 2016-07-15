f140a45ea1e746988db4b9c12396d87b6a6c425c
deadmeat <robin.t.potter@googlemail.com>
2016-07-15 21:03:00 +0100

    a1

--- myappcam1/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/deadmeat/Spod/unfinished-games/myappcam1/bin/AndroidManifest.xml : \
-/home/deadmeat/Spod/unfinished-games/myappcam1/AndroidManifest.xml \
--- myappcam1/bin/stopmotioncamera-release-unsigned.apk.d
+++
@@ -1,3 +0,0 @@
-/home/deadmeat/Spod/unfinished-games/myappcam1/bin/stopmotioncamera-release-unsigned.apk : \
-/home/deadmeat/Spod/unfinished-games/myappcam1/bin/stopmotioncamera.ap_ \
-/home/deadmeat/Spod/unfinished-games/myappcam1/bin/classes.dex \
