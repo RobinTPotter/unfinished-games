9c8b2fb829ca31f8462d7a68925a8274a3ee892d
RobinTPotter <r@p.com>
2016-08-12 14:49:01 +0100

    pod

--- finger/src/robin/hello/finger/TheFinger.java
+++ finger/src/robin/hello/finger/TheFinger.java
@@ -67,7 +67,7 @@ public class TheFinger extends Activity implements View.OnTouchListener, Gesture
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
 
         plate.setRad(
-                plate.getRad() + 2f);
+                plate.getRad() * 5);
         plate.setM((int) (e2.getX()), (int) (e2.getY()));
 
         if  (Math.abs(distanceX)>Math.abs( distanceY)) plate.setCol(Color.YELLOW); else plate.setCol(Color.GRAY);
