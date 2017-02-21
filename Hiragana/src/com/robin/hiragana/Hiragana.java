a58f3389face8bbb8a99d075376f15d52df718cc
RobinTPotter <r@p.com>
2017-02-21 18:52:02 +0000

    orientation change and no restart on turn

--- Hiragana/src/com/robin/hiragana/Hiragana.java
+++ Hiragana/src/com/robin/hiragana/Hiragana.java
@@ -1,12 +1,14 @@
 package com.robin.hiragana;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -24,6 +26,9 @@ public class Hiragana extends Activity {
 
     String[] hiraganaList;
 
+    static String SCORE_WRONG_KEY = "score_wrong";
+    static String SCORE_CORRECT_KEY = "score_correct";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -31,6 +36,11 @@ public class Hiragana extends Activity {
         setContentView(R.layout.main);
 
         hiraganaList = getString(R.string.hiragana).split(",");
+
+        if (savedInstanceState != null) {
+            score_wrong = savedInstanceState.getInt(SCORE_WRONG_KEY, 0);
+            score_correct = savedInstanceState.getInt(SCORE_CORRECT_KEY, 0);
+        }
 
         final Button buttonReset = (Button) findViewById(R.id.button_reset);
         buttonReset.setOnClickListener(new View.OnClickListener() {
@@ -43,11 +53,11 @@ public class Hiragana extends Activity {
 
         buttons = new Button[hiraganaList.length];
 
-        for (int hh=0; hh<hiraganaList.length;hh++) {
-            String text=hiraganaList[hh];
-            Button b =(Button)findViewById(getResources().getIdentifier("button_" + text, "id",getPackageName()));
+        for (int hh = 0; hh < hiraganaList.length; hh++) {
+            String text = hiraganaList[hh];
+            Button b = (Button) findViewById(getResources().getIdentifier("button_" + text, "id", getPackageName()));
             b.setText(text);
-            final int  _hh=hh;
+            final int _hh = hh;
             b.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     if (current_hiragana == _hh) score_correct++;
@@ -58,9 +68,38 @@ public class Hiragana extends Activity {
             });
         }
 
+        showScore();
         nextHiragana();
 
     }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+
+        score_correct = savedInstanceState.getInt(SCORE_CORRECT_KEY, 0);
+        score_wrong = savedInstanceState.getInt(SCORE_WRONG_KEY, 0);
+        showScore();
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        outState.putInt(SCORE_CORRECT_KEY, score_correct);
+        outState.putInt(SCORE_WRONG_KEY, score_wrong);
+
+        super.onSaveInstanceState(outState);
+
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        
+    }
+
 
     /**
      * read the list of characters in the strings resource R.string.hiragana
@@ -85,12 +124,16 @@ public class Hiragana extends Activity {
 
     }
 
+    /**
+     * updates the score labels based on the internal numbers
+     */
     private void showScore() {
 
         final TextView textCorrect = (TextView) findViewById(R.id.textCorrect);
         final TextView textWrong = (TextView) findViewById(R.id.textWrong);
         textCorrect.setText(String.valueOf(score_correct));
         textWrong.setText(String.valueOf(score_wrong));
+
     }
 
 }
