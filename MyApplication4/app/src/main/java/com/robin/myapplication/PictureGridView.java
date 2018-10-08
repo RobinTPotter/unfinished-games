07666d7047e215a644f17cce2568cb58ef85a015
robin <robin.potter@experian.com>
2018-10-08 13:10:35 +0100

    p92

--- MyApplication4/app/src/main/java/com/robin/myapplication/PictureGridView.java
+++ MyApplication4/app/src/main/java/com/robin/myapplication/PictureGridView.java
@@ -15,12 +15,11 @@ import android.widget.Toast;
 public class PictureGridView extends ImageView {
 
 
-
     private int offsetx = 0;
     private int offsety = 0;
 
-    private int r=0;
-    private int c=0;
+    private int r = 0;
+    private int c = 0;
 
     private Bitmap bitmap;
 
@@ -28,72 +27,72 @@ public class PictureGridView extends ImageView {
 
     Activity parentActivity;
 
-    public PictureGridView(Context c){
+    public PictureGridView(Context c) {
         super(c);
     }
 
     public void setParentActivity(Activity a) {
-        parentActivity=a;
+        parentActivity = a;
     }
 
 
     public void setScaleFactor(float sf) {
 
-        mScaleFactor=sf;
+        mScaleFactor = sf;
     }
 
     public void onDraw(Canvas canvasx) {
+        if (parentActivity == null) return;
+        Toast.makeText(parentActivity, "going to draw grid", Toast.LENGTH_SHORT).show();
+        if (bitmap == null) {
+            Toast.makeText(parentActivity, "bmp is null", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (!bitmap.isMutable()) {
+            Toast.makeText(parentActivity, "bmp is not mutable", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-            Toast.makeText(parentActivity, "going to draw grid", Toast.LENGTH_SHORT).show();
-            if (bitmap == null) {
-                Toast.makeText(parentActivity, "bmp is null", Toast.LENGTH_SHORT).show();
-                return;
-            } else if (!bitmap.isMutable()) {
-                Toast.makeText(parentActivity, "bmp is not mutable", Toast.LENGTH_SHORT).show();
-                return;
+        // pictureView.setImageURI(Uri.fromFile(new File(currentPicture)));
+        // Bitmap griddedBitmap = Bitmap.createBitmap(bitmap);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.scale(mScaleFactor, mScaleFactor);
+
+        Toast.makeText(parentActivity, "canvas is " + canvas.toString(), Toast.LENGTH_SHORT).show();
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStrokeWidth(1.0f);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
+
+        Toast.makeText(parentActivity, "set paints etc", Toast.LENGTH_SHORT).show();
+
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        Toast.makeText(parentActivity, "" + width + "," + height, Toast.LENGTH_SHORT).show();
+
+        for (int cc = 0; cc < c; cc++) {
+            for (int rr = 0; rr < r; rr++) {
+                Rect rect = new Rect(offsetx + cc * width, offsety + rr * height, offsetx + (cc + 1) * width - 1, offsety + (rr + 1) * height - 1);
+
+                Toast.makeText(parentActivity, "" + rect, Toast.LENGTH_SHORT).show();
+                canvas.drawRect(rect, paint);
             }
+        }
 
-            // pictureView.setImageURI(Uri.fromFile(new File(currentPicture)));
-            // Bitmap griddedBitmap = Bitmap.createBitmap(bitmap);
-            Canvas canvas = new Canvas(bitmap);
-            canvas.scale(mScaleFactor, mScaleFactor);
-
-            Toast.makeText(parentActivity, "canvas is " + canvas.toString(), Toast.LENGTH_SHORT).show();
-
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            paint.setStrokeWidth(0.5f);
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLACK);
-
-            Toast.makeText(parentActivity, "set paints etc", Toast.LENGTH_SHORT).show();
-
-            int width = bitmap.getWidth();
-            int height = bitmap.getHeight();
-
-            Toast.makeText(parentActivity, "" + width + "," + height, Toast.LENGTH_SHORT).show();
-
-            for (int cc = 0; cc < c; cc++) {
-                for (int rr = 0; rr < r; rr++) {
-                    Rect rect = new Rect(offsetx + cc * width, offsety + rr * height, offsetx + (cc + 1) * width - 1, offsety + (rr + 1) * height - 1);
-
-                    Toast.makeText(parentActivity, "" + rect, Toast.LENGTH_SHORT).show();
-                    canvas.drawRect(rect, paint);
-                }
-            }
-
-            this.setImageBitmap(bitmap);
+        this.setImageBitmap(bitmap);
         super.onDraw(canvasx);
 
     }
 
     public void setRowsColumns(int r, int c) {
-        this.r=r;
-        this.c=c;
+        this.r = r;
+        this.c = c;
         invalidate();
     }
 
     public void setImageBitmap(Bitmap bitmap) {
         super.setImageBitmap(bitmap);
-        this.bitmap=bitmap;
+        this.bitmap = bitmap;
     }
 }
